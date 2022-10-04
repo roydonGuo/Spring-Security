9a119ebeadff839d67df4a3bdf897625e4de4747
roydonGuo <3133010060@qq.com>
2022-10-04 19:56:22 +0800

    fastjson

--- TokenDemo1/src/main/java/com/roydon/tokendemo1/utils/FastJsonRedisSerializer.java
+++ TokenDemo1/src/main/java/com/roydon/tokendemo1/utils/FastJsonRedisSerializer.java
@@ -10,6 +10,7 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.SerializationException;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Redis使用FastJson序列化
@@ -18,7 +19,7 @@ import java.nio.charset.Charset;
  */
 public class FastJsonRedisSerializer<T> implements RedisSerializer<T> {
 
-    public static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
+    public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
 
     private Class<T> clazz;
 
