d35a1aeafc483377ffd56806849dbd73492a9dbd
roydonGuo <3133010060@qq.com>
2022-10-04 20:29:03 +0800

    跨域测试#

--- TokenDemo1/src/main/java/com/roydon/tokendemo1/controller/HelloController.java
+++ TokenDemo1/src/main/java/com/roydon/tokendemo1/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.roydon.tokendemo1.controller;
 
+import com.roydon.tokendemo1.common.ResponseResult;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.roydon.tokendemo1.common.CodeConstants.CODE_200;
 
 /**
  * Created by Intellij IDEA
@@ -23,6 +26,15 @@ public class HelloController {
     @PreAuthorize("hasAuthority('system:dept:list')")
     public String hello() {
         return "Hello World~";
+    }
+
+    /**
+     * 测试跨域
+     * @return
+     */
+    @RequestMapping("/testCors")
+    public ResponseResult testCors(){
+        return new ResponseResult(CODE_200,"testCors");
     }
 
 }
--- vue_security_cors/README.md
+++ vue_security_cors/README.md
@@ -1,8 +1,11 @@
-# sg_security_demo
+# 测试跨域的vue页面
 
-电脑中安装过npm环境后
 
-在sg_security_demo目录下执行npm install下载依赖
-
-下载完后执行npm run serve启动项目
+```shell
+npm i #安装依赖
+```
+```shell
+npm run serve #启动
+```
+### 访问给出的端口
 
